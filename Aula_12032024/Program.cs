68301368c8b3d17c8023552493882102fc89e033
Usuario <Usuario@CPD02-M18>
2024-03-12 11:47:15 -0300

    11:47

--- Aula_12032024/Program.cs
+++ Aula_12032024/Program.cs
@@ -10,47 +10,59 @@ namespace Aula_12032024
     {
         static void Main(string[] args)
         {
-            //entrada
+            //Cáuculo IMC
+            ////entrada
 
-            Console.Write("Peso: ");
-            double p = double.Parse(Console.ReadLine());
+            //Console.Write("Peso: ");
+            //double p = double.Parse(Console.ReadLine());
 
-            Console.Write("Altura: ");
-            double a = double.Parse(Console.ReadLine());
+            //Console.Write("Altura: ");
+            //double a = double.Parse(Console.ReadLine());
 
-            //Processamento e saída
-            double imc = p/(a*a);
+            ////Processamento e saída
+            //double imc = p/(a*a);
 
-            Console.Write("imc: "+imc+". ");
+            //Console.Write("imc: "+imc+". ");
 
-            if (imc<=16.9) {
-                Console.WriteLine("Muito abaixo do peso");
-            }
-            else if (imc <= 18.4)
-            {
-                Console.WriteLine("abaixo do peso");
-            }
-            else if (imc <= 24.9)
-            {
-                Console.WriteLine("peso normal");
-            }
-            else if (imc <= 29.9)
-            {
-                Console.WriteLine("acima do peso");
-            }
-            else if (imc <= 34.9)
-            {
-                Console.WriteLine("obesidade grau 1");
-            }
-            else if (imc <= 40)
-            {
-                Console.WriteLine("obesidade grau 2");
-            }
-            else
-            {
-                Console.WriteLine("obesidade grau 3");
-            }
-            Console.ReadKey();
+            //if (imc <= 16.9)
+            //{
+            //    Console.WriteLine("Muito abaixo do peso");
+            //}
+            //else if (imc <= 18.4)
+            //{
+            //    Console.WriteLine("abaixo do peso");
+            //}
+            //else if (imc <= 24.9)
+            //{
+            //    Console.WriteLine("peso normal");
+            //}
+            //else if (imc <= 29.9)
+            //{
+            //    Console.WriteLine("acima do peso");
+            //}
+            //else if (imc <= 34.9)
+            //{
+            //    Console.WriteLine("obesidade grau 1");
+            //}
+            //else if (imc <= 40)
+            //{
+            //    Console.WriteLine("obesidade grau 2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("obesidade grau 3");
+            //}
+            //Console.ReadKey();
+
+            //analise de aprovação
+            //entrada e declarações
+            Console.Write("media do aluno: ");
+            double M = double.Parse(Console.ReadLine());
+
+            Console.Write("Porcentagem de presença: ");
+            int P = int.Parse(Console.ReadLine());
+
+            //processamento e saída
         }
     }
 }
